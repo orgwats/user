9c6bdaae040bc4ea9bcc83c748418ed1450ef011
waik1ki <arbutus@outlook.kr>
2025-04-08 00:01:07 +0900

    feat: 유저 Repository 추가

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,34 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (e: any) {
+      if (e.code === '23505') {
+        throw new ConflictException();
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
